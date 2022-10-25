51a7b150cc7035aafab1e3daf54aaae2e1f8fda0
Mustafa Alperen Karakuş <alperenkarakus062002@gmail.com>
2022-10-25 17:47:42 +0300

    blog bugfix + tag html css complated

--- components/tags.tsx
+++ components/tags.tsx
@@ -1,5 +1,40 @@
-export default function Tags(){
-    return(
-        <div>Tags</div>
+import Link from "next/link"
+import style from "../styles/tags.module.scss"
+
+export default function Tags() {
+    const tags = [
+        "Technology",
+        "Open Source",
+        "JavaScript",
+        "Minimalism",
+        "Self-help",
+        "Animals",
+        "Herbivores",
+        "HTML",
+        "CSS",
+        "PHP",
+        "Web Technologies",
+        "Career",
+        "Life",
+        "Spirituality",
+        "Food",
+        "Cooking",
+        "Sports",
+        "Racing",
+        "Mountain Hiking",
+        "Cruising"
+    ]
+
+    const everyTag = tags.map((item)=>{
+        return <li><Link href="/category">{item}</Link></li>
+    })
+
+    return (
+        <div className={style.tags}>
+            <h1>tags</h1>
+            <ul>
+                {everyTag}
+            </ul>
+        </div>
     )
 }
