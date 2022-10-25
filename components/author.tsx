99a4cdbb1a19dfdd9335aa0c9c993f5c533463d4
Mustafa Alperen Karakuş <alperenkarakus062002@gmail.com>
2022-10-25 14:29:30 +0300

    author + hero bugfix

--- components/author.tsx
+++ components/author.tsx
@@ -1,5 +1,13 @@
+import style from "../styles/author.module.scss"
+import Image from "next/image"
 export default function Author(){
+    const profilePhoto = "https://firebasestorage.googleapis.com/v0/b/omnio-eb0f4.appspot.com/o/ProfilePhoto.png?alt=media&token=d72c9d57-7ecc-41cc-b2ed-43c0f632a2df"
     return(
-        <div>Author</div>
-    )
+        <div className={style.author}>
+            <div>by</div> 
+            <Image src={profilePhoto} width={32} height={32}/>
+            <div className={style.authorName}>Andrea Wise</div>
+            <span className={style.articleReadTime}>(4 mins read)</span>
+        </div>
+        )
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -7,15 +7,15 @@ export default function Hero() {
     return (
         <div className={style.hero} style={{ background: "url(https://firebasestorage.googleapis.com/v0/b/omnio-eb0f4.appspot.com/o/ArticleImage.png?alt=media&token=3866f189-62bb-428e-bfe1-e06be72f789a)" }}>
 
-            <div>
+            <div className={style.card}>
                 <h4>FEATURED ARTICLE</h4>
                 <h1>World’s Most Dangerous Technology Ever Made.</h1>
                 <Author />
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                <span>
+                <div className={style.buttonGroup}>
                     <button>Read more</button>
-                </span>
+                </div>
             </div>
 
         </div>
