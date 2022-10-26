f361da258904d38536ee350b6394faed58e94bdc
Mustafa Alperen Karakuş <alperenkarakus062002@gmail.com>
2022-10-26 14:53:43 +0300

    connection of blog and author component to firebase complated

--- components/author.tsx
+++ components/author.tsx
@@ -1,13 +1,12 @@
 import style from "../styles/author.module.scss"
 import Image from "next/image"
-export default function Author(){
-    const profilePhoto = "https://firebasestorage.googleapis.com/v0/b/omnio-eb0f4.appspot.com/o/ProfilePhoto.png?alt=media&token=d72c9d57-7ecc-41cc-b2ed-43c0f632a2df"
+export default function Author(props: any){
     return(
         <div className={style.author}>
             <div>by</div> 
-            <Image src={profilePhoto} width={32} height={32}/>
-            <div className={style.authorName}>Andrea Wise</div>
-            <span className={style.articleReadTime}>(4 mins read)</span>
+            <Image src={props.authorPhoto} width={32} height={32}/>
+            <div className={style.authorName}>{props.authorName}</div>
+            <span className={style.articleReadTime}>({props.minRead})</span>
         </div>
         )
 }
--- components/blog.tsx
+++ components/blog.tsx
@@ -2,66 +2,16 @@ import Tags from "./tags";
 import Image from "next/image";
 import Author from "./author";
 import style from "../styles/blog.module.scss"
+import { getPosts } from "../services/post.service";
 export default function Blog(){
-
-   const data = [
-          {   
-            "minRead":5,
-            "id": 1,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "text": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-            "author":"Andrea Wise",
-            "backgroundImage": "https://firebasestorage.googleapis.com/v0/b/omnio-eb0f4.appspot.com/o/ArticleImage.png?alt=media&token=3866f189-62bb-428e-bfe1-e06be72f789a"
-        },
-        {   
-            "minRead":5,
-            "id": 2,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "text": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-            "author":"Andrea Wise",
-            "backgroundImage": "https://firebasestorage.googleapis.com/v0/b/omnio-eb0f4.appspot.com/o/ArticleImage.png?alt=media&token=3866f189-62bb-428e-bfe1-e06be72f789a"
-        },
-        {   
-            "minRead":5,
-            "id": 3,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "text": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-            "author":"Andrea Wise",
-            "backgroundImage": "https://firebasestorage.googleapis.com/v0/b/omnio-eb0f4.appspot.com/o/ArticleImage.png?alt=media&token=3866f189-62bb-428e-bfe1-e06be72f789a"
-        },
-        {   
-            "minRead":5,
-            "id": 1,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "text": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-            "author":"Andrea Wise",
-            "backgroundImage": "https://firebasestorage.googleapis.com/v0/b/omnio-eb0f4.appspot.com/o/ArticleImage.png?alt=media&token=3866f189-62bb-428e-bfe1-e06be72f789a"
-        },
-        {   
-            "minRead":5,
-            "id": 2,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "text": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-            "author":"Andrea Wise",
-            "backgroundImage": "https://firebasestorage.googleapis.com/v0/b/omnio-eb0f4.appspot.com/o/ArticleImage.png?alt=media&token=3866f189-62bb-428e-bfe1-e06be72f789a"
-        },
-        {   
-            "minRead":5,
-            "id": 3,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "text": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-            "author":"Andrea Wise",
-            "backgroundImage": "https://firebasestorage.googleapis.com/v0/b/omnio-eb0f4.appspot.com/o/ArticleImage.png?alt=media&token=3866f189-62bb-428e-bfe1-e06be72f789a"
-        },
-        ]
-
-       const cards = data.map((card)=>{
+       
+       const cards = getPosts().map((card)=>{   
         return(
-            <div className={style.card}>
-                <Image src={card.backgroundImage} width={400} height={200} />
-                <Author />
-                <h1>{card.title}</h1>
-                <p>{card.text}</p>
+            <div className={style.card} key= {card.id}>
+                <Image src={card?.backgroundImageURL} width={400} height={200} />
+                <Author {...{...card.author ,"minRead" : card.minRead }} key= {card.id}/>
+                <h1>{card?.title}</h1>
+                <p>{card?.text.substring(0,300)}...</p>
                 <div className={style.buttonGroup}>
                     <button>Read More</button>
                 </div>
